35d0f25f33c0e9d85a05718f68e2393e02da47be
yxj <564197835@qq.com>
2021-09-02 13:07:31 +0800

    Initial Commit

---
+++ StudentBook/StudentBookApp.swift
@@ -0,0 +1,15 @@
+// StudentBookApp.swift
+
+import SwiftUI
+
+@main
+struct StudentBookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
